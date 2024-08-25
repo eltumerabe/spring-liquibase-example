38d1a47ecd412f319ebca393d7857d0c07e09b64
eltumerabe <eltumerabe@gmail.com>
2024-08-25 20:39:25 +0400

    liquibase with spring boot

---
+++ src/test/java/com/example/springliquibaseexample/SpringLiquibaseExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springliquibaseexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLiquibaseExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
